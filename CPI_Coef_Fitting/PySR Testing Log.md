d2c8248949e5ce24b567e738c0b843a52f500d5b
GongJr0 <guneykiymac@gmail.com>
2025-03-08 21:33:06 +0100

    Commit

--- CPI_Coef_Fitting/PySR Testing Log.md
+++
@@ -1,9 +0,0 @@
-## Symbolic Regression with PySR
-
-`PySR` is an open source library that allows fitting complex equations that model the given dataset. By achieving a 
-mathematical representation of a given dataset, we can contextualize, interpret and simplify our approach to relational
-modelling. In case of MacroSim, PySR is being tested as a tool of modelling CPI through the parameters that are tracked
-within the simulation engine. If successful, this will allow the creation of a simple, relational expression that is
-generated once per distinct run, before the simulation loop begins.
-
-__Logs and updates will be added as development continues__
--- MacroSim/SimEngine.py
+++ MacroSim/SimEngine.py
@@ -1,20 +1,5 @@
-from typing import Union
-
-import numpy as np
-from Logger import Logger
-from IS_LM import IS_LM
 
 
-class Engine:
-
-    def __init__(self, sr_model: Union[IS_LM, ...], lr_model: ..., entropy_variance: int = 1, random_seed: int = 0):
-        self.logger = Logger()
-
-        self.sr_model = sr_model
-        self.lr_model = lr_model
-
-        # scale represents standard deviation, __init__ takes variance to follow normal distribution notation conventions
-        self.entropy_dist = np.random.normal(loc=0, scale=np.sqrt(entropy_variance), size=self.sr_model.shape)
-
-    def simulate(self, steps: int) -> None:
+class SimEngine:
+    def __init__(self):
         ...
