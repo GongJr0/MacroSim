ba1054fddb2c212267df835bdfcd2dca34ed228b
GongJr0 <guneykiymac@gmail.com>
2025-04-25 01:02:59 +0200

    Parallel search test

--- test/engine_test.py
+++ test/engine_test.py
@@ -17,8 +17,8 @@ start = dt.datetime.fromisoformat('2002-01-01')
 end = dt.datetime.fromisoformat('2024-12-31')
 
 df = fred.get_series(
-    series_ids=['LRAC64TTUSQ156S', 'CPIAUCSL', 'M2SL', 'GDPC1'],
-    series_alias=['LABPART', 'CPI', 'M2', 'RGDP'],
+    series_ids=['PCECC96', 'GPDIC1', 'EXPGSC1', 'IMPGSC1', 'GDPC1'],
+    series_alias=['CONSUMPTION', 'DOMINV', 'REXP', 'RIMP', 'RGDP'],
     date_range=(start, end),
     reindex_freq='QS'
 )
@@ -28,26 +28,23 @@ gd = GrowthDetector(
 )
 growth = gd.compose_estimators(cv=2)
 
-print(growth['LABPART'].get_best()['sympy_format'])
-print(growth['CPI'].get_best()['sympy_format'])
+eqsr = EqSearch(
+    X=df.drop('RGDP', axis=1),
+    y=df['RGDP']
+)
+eqsr.distil_split()
+eqsr.search(maxsize=16, niterations=250)
 
-# eqsr = EqSearch(
-#     X=df.drop('RGDP', axis=1),
-#     y=df['RGDP']
-# )
-# eqsr.distil_split()
-# eqsr.search()
-#
-# init_params = {
-#     var: (df[var].head(gd.get_lag_count), growth[var]) for var in df.drop('RGDP', axis=1).columns
-# }
-# engine = SimEngine(
-#     sr=eqsr.get_model,
-#     init_params=init_params,
-#     n_lags=gd.get_lag_count
-# )
-#
-# out = engine.simulate(50)
-#
-# with open('out.pkl', 'wb') as f:
-#     pickle.dump(out, f)  # type:ignore
+init_params = {
+    var: (df[var].head(gd.get_lag_count), growth[var]) for var in df.drop('RGDP', axis=1).columns
+}
+engine = SimEngine(
+    sr=eqsr.get_model,
+    init_params=init_params,
+    n_lags=gd.get_lag_count
+)
+
+out = engine.simulate(50)
+
+with open('out.pkl', 'wb') as f:
+    pickle.dump(out, f)  # type:ignore
